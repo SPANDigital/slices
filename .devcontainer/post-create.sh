e4fd57609776b21069ff97ee6a4615363fc6d12a
Richard Wooding <richard.wooding@spandigital.com>
2024-11-21 16:38:38 +0200

    Commit lint

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Running inside docker
+if ! [ -f "/.dockerenv" ]
+then
+    echo "Error: This script must be run inside a docker container"
+    exit 1
+fi
+
+source .env
+
+# Git Configuration
+git config --global --add safe.directory ${localWorkspaceFolder}
+
+git config --global user.name "${GITHUB_USER}"
+git config --global user.email "${GITHUB_EMAIL}"
+
+. ${NVM_DIR}/nvm.sh
+nvm install --lts
+npm install -g @commitlint/cli @commitlint/config-conventional
+pre-commit install
