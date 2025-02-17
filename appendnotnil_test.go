6ab93c504bca1e17023d146ca73587d0ef606636
Richard Wooding <richard.wooding@spandigital.com>
2025-02-17 10:36:04 +0200

    Chore: added appendnotnil BDD tests

--- appendnotnil_test.go
+++ appendnotnil_test.go
@@ -1,35 +1,113 @@
 package slices
 
 import (
-	"reflect"
+	"context"
+	"errors"
+	"github.com/cucumber/godog"
+	"regexp"
 	"testing"
 )
 
-func TestAppendNotNil(t *testing.T) {
-	type args[T any] struct {
-		in     []*T
-		values []*T
+type firstArgKey struct{}
+type resultKey struct{}
+
+var naiveStringPattern = regexp.MustCompile(`"([^"]*)"`)
+
+func tableToStringPtrSlice(t *godog.Table) []*string {
+	var out []*string
+	for _, row := range t.Rows {
+		for _, cell := range row.Cells {
+			switch value := cell.Value; {
+			case value == "nil":
+				out = append(out, nil)
+			case naiveStringPattern.MatchString(value):
+				m := naiveStringPattern.FindStringSubmatch(value)
+				out = append(out, &m[1])
+			default:
+				out = append(out, &value)
+			}
+		}
 	}
-	type testCase[T any] struct {
-		name    string
-		args    args[T]
-		wantOut []*T
+	return out
+}
+
+func anEmptySlice(ctx context.Context) (context.Context, error) {
+	return context.WithValue(ctx, firstArgKey{}, []*string{}), nil
+}
+
+func aSliceWithElements(ctx context.Context, elements *godog.Table) (context.Context, error) {
+	return context.WithValue(ctx, firstArgKey{}, tableToStringPtrSlice(elements)), nil
+}
+
+func iCallAppendNotNilOnTheSliceWithTheFollowingVariableArguments(ctx context.Context, varArgs *godog.Table) (context.Context, error) {
+	firstArg, ok := ctx.Value(firstArgKey{}).([]*string)
+	if !ok {
+		return ctx, errors.New("first arg not found in context")
 	}
-	tests := []testCase[string]{
-		{
-			name: "empty",
-			args: args[string]{
-				in:     []*string{ptr("a"), ptr("b")},
-				values: []*string{ptr("c"), nil, ptr("d")},
-			},
-			wantOut: []*string{ptr("a"), ptr("b"), ptr("c"), ptr("d")},
+	result := AppendNotNil(firstArg, tableToStringPtrSlice(varArgs)...)
+	return context.WithValue(ctx, resultKey{}, result), nil
+}
+
+func iCallAppendNotNilOnTheSlice(ctx context.Context) (context.Context, error) {
+	firstArg, ok := ctx.Value(firstArgKey{}).([]*string)
+	if !ok {
+		return ctx, errors.New("first arg not found in context")
+	}
+	result := AppendNotNil(firstArg)
+	return context.WithValue(ctx, resultKey{}, result), nil
+}
+
+func theResultShouldBeASliceWithElements(ctx context.Context, t *godog.Table) (context.Context, error) {
+	result, ok := ctx.Value(resultKey{}).([]*string)
+	if !ok {
+		return ctx, errors.New("result not found in context")
+	}
+	expected := tableToStringPtrSlice(t)
+	if len(result) != len(expected) {
+		return ctx, errors.New("result length does not match expected length")
+	}
+	for i := range result {
+		if (result[i] == nil && expected[i] != nil) || (result[i] != nil && expected[i] == nil) {
+			return ctx, errors.New("nil mismatch")
+		}
+		if result[i] != nil && *result[i] != *expected[i] {
+			return ctx, errors.New("value mismatch")
+		}
+	}
+	return ctx, nil
+}
+
+func theResultShouldBeAnEmptySlice(ctx context.Context) (context.Context, error) {
+	result, ok := ctx.Value(resultKey{}).([]*string)
+	if !ok {
+		return ctx, errors.New("result not found in context")
+	}
+	if len(result) != 0 {
+		return ctx, errors.New("result is not empty")
+	}
+	return ctx, nil
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	ctx.Step(`^an empty slice$`, anEmptySlice)
+	ctx.Step(`^a slice with elements$`, aSliceWithElements)
+	ctx.Step(`^I call AppendNotNil on the slice with the following variable arguments$`, iCallAppendNotNilOnTheSliceWithTheFollowingVariableArguments)
+	ctx.Step(`^I call AppendNotNil on the slice$`, iCallAppendNotNilOnTheSlice)
+	ctx.Step(`^the result should be a slice with elements$`, theResultShouldBeASliceWithElements)
+	ctx.Step(`^the result should be an empty slice$`, theResultShouldBeAnEmptySlice)
+}
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t, // Testing instance that will run subtests.
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if gotOut := AppendNotNil(tt.args.in, tt.args.values...); !reflect.DeepEqual(gotOut, tt.wantOut) {
-				t.Errorf("AppendNotNil() = %v, want %v", gotOut, tt.wantOut)
-			}
-		})
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
 	}
 }
---
+++ features/appendnotnil.feature
@@ -0,0 +1,53 @@
+Feature: AppendNotNil
+
+Scenario: Append elements to a slice
+  Given a slice with elements
+    | "a"     |
+    | "b"     |
+    | nil     |
+    | "c"     |
+    | nil     |
+  When I call AppendNotNil on the slice with the following variable arguments
+    | "d"     |
+    | "e"     |
+    | nil     |
+    | "f"     |
+  Then the result should be a slice with elements
+    | "a"     |
+    | "b"     |
+    | nil     |
+    | "c"     |
+    | nil     |
+    | "d"     |
+    | "e"     |
+    | "f"     |
+
+  Scenario: Append non-nil elements to an empty slice
+    Given an empty slice
+    When I call AppendNotNil on the slice with the following variable arguments
+      | "d"     |
+      | "e"     |
+      | nil     |
+      | "f"     |
+    Then the result should be a slice with elements
+      | "d"     |
+      | "e"     |
+      | "f"     |
+
+  Scenario: Append non-nil elements to a slice with all nil elements
+    Given a slice with elements
+      | nil     |
+      | nil     |
+      | nil     |
+    When I call AppendNotNil on the slice with the following variable arguments
+      | "x" |
+      | "y" |
+      | nil |
+      | "z" |
+    Then the result should be a slice with elements
+      | nil |
+      | nil |
+      | nil |
+      | "x" |
+      | "y" |
+      | "z" |
